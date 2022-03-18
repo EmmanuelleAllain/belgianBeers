7d4686587ef94936571e740ed91b69b519a4b3a4
EmmanuelleAllain <e.allain13@gmail.com>
2022-03-18 13:46:54 +0100

    beginning of js

--- page4.js
+++ page4.js
@@ -15,3 +15,26 @@ form.addEventListener ("submit", function(event) {
   confirmationText.innerText = `Merci ${firstName.value} ! Votre inscription a bien été prise en compte voici le détail de votre réservation : Nom : ${name.value} Pénom : ${firstName.value} Evénement choisi : ${eventSelect.value} Nombre de personnes : ${numberSelect.value}`;
   confirmationText.classList.add('confirmation-text');
 });
+
+// ELEMENTS POUR LA BIERE QUI SUIT LA SOURIS
+
+const beerIcon = document.querySelector("beer-icon");
+const beerIconMousemove = document.querySelector("beer-icon-mousemove");
+/*document.addEventListener = ('mousemove', function(event){
+  $("#beer-icon-mousemove").animate({event.clientX, event.clientY});
+})*/
+
+
+window.addEventListener("mousemove", (event) => {
+  beerIcon.classList.toggle("beerIconMousemove")
+  
+  console.log(`Position de la souris : X = ${event.clientX} | Y = ${event.clientY}`);
+  /*mousemove.style.left = e.pageX + "px";
+  mousemove.style.top = e.pageY + "px";*/
+});
+/*window.addEventListener("mousedown", () => {
+  mousemove.style.transform = "scale(2) translate(-25%, -25%)";
+});
+window.addEventListener("mouseup", () => {
+  mousemove.style.transform = "scale(1) translate(-50%, -50%)";
+});*/
