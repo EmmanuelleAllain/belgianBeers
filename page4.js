d2544cfce08ee851b1e52fecb5b3996fb41d333d
EmmanuelleAllain <e.allain13@gmail.com>
2022-03-23 14:49:30 +0100

    modifs affichage réponses formulaire

--- page4.js
+++ page4.js
@@ -1,19 +1,25 @@
 // ELEMENTS POUR LE FORMULAIRE
 const form = document.querySelector("form");
 const confirmationText = document.createElement('p');
-const confirmationDiv = document.getElementById('confirmation-container');
+const confirmationDiv = document.querySelector("#conf-div");
 const firstName = document.getElementById("firstName");
 const name = document.getElementById("name");
 const email = document.getElementById("email");
 const eventSelect = document.getElementById("event-select");
 const numberSelect = document.getElementById("number-select");
 
-// Au clic, les éléments sélectionnés par l'internaute apparaisse sous le formulaire
+//element pour le bouton refresh formulaire
+const buttonRefresh = document.createElement('button');
+
+// Au clic, les éléments sélectionnés par l'internaute apparaissent et le formulaire disparait
 form.addEventListener ("submit", function(event) {
   event.preventDefault();
+  form.remove();
+  confirmationDiv.classList.add("confirmation-container");
   confirmationDiv.appendChild(confirmationText);
   confirmationText.classList.add("conf-text");
   confirmationText.innerHTML = `<strong>Merci ${firstName.value}</strong> !<br>Voici le détail de votre réservation :<br> Nom : ${name.value} <br>Pénom : ${firstName.value}<br>Evénement choisi : ${eventSelect.value}<br>Nombre de personnes : ${numberSelect.value}`;
-  confirmationText.classList.add('confirmation-text');
+  confirmationDiv.appendChild(buttonRefresh);
+  buttonRefresh.innerHTML = `<a href="indexPage4.html">Retour</a>`;
+  buttonRefresh.classList.add("button-refresh");
 });
-
